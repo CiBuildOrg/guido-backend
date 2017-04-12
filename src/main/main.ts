ec6c179750786ca832063fa783c47714c8f1403d
Charles Samborski <demurgos@demurgos.net>
2017-04-12 07:30:55 +0200

    Expose an HTTP interface for the API
    Signed-off-by: Charles Samborski <demurgos@demurgos.net>

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,7 +1,16 @@
-import * as api from "../lib/api";
+import express = require("express");
+import {Server} from "http";
+import morgan = require("morgan");
+import {apiRouter} from "./routes";
 
 async function run() {
-  console.log(await api.getUsers());
+  const app: express.Application = express();
+  app.use(morgan("dev"));
+  app.use(apiRouter);
+
+  const server: Server = app.listen(8080, () => {
+    console.log(`Listening on: http://localhost:${server.address().port}`);
+  });
 }
 
 run();
