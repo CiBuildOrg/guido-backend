8a383d0710014be3e1a5b7330a6a12fe94a9a173
Charles Samborski <demurgos@demurgos.net>
2017-04-12 07:35:12 +0200

    Enable Travis CI
    Signed-off-by: Charles Samborski <demurgos@demurgos.net>

--- src/main/routes.ts
+++ src/main/routes.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router} from "express";
+import {Request, Response, Router} from "express";
 
 import * as api from "../lib/api";
 
@@ -6,7 +6,7 @@ export const apiRouter: Router = Router();
 
 apiRouter.get("/users/:user_id", async function (req: Request, res: Response) {
   const userId: string = req.params["user_id"];
-  res.status(200).json({name: "Test user", friends : await api.getUsers()});
+  res.status(200).json({name: "Test user", friends: await api.getUsers()});
 });
 
 export default apiRouter;
